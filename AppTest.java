bd61622b13762a6b8dca009892c52f546eb4f2d4
root <root@ip-172-31-41-154.ec2.internal>
2024-12-14 18:19:02 +0000

    Added AppTest.java

---
+++ AppTest.java
@@ -0,0 +1,14 @@
+package com.example.jenkinspipeline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true, "Basic Test Passed");
+    }
+}
+
